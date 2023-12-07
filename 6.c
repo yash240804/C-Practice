42874bbf2aa14df2f7ff4b0706d3abd76c0f8f62
Yash <yashbisen24@gmail.com>
2023-12-07 14:56:49 +0530

    6th problem

---
+++ 6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int l, b;
+    printf("Enter Lenght Of Rectangle\n");
+    scanf("%d", &l);
+    printf("Enter Breadht Of Rectangle\n");
+    scanf("%d", &b);
+    printf("Are Of Rectangle Is : %d", l * b);
+
+    return 0;
+}
