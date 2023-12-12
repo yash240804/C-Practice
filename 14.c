3e1af605658ecaa02e74fb54a17118aa1b950e66
Yash <yashbisen24@gmail.com>
2023-12-12 21:36:27 +0530

    14th problem

---
+++ 14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, rem, div, sum = 0;
+    printf("Enter the no. you want to sum of its digit\n");
+    scanf("%d", &n);
+    while (n != 0)
+    {
+        rem = n % 10;
+        sum += rem;
+        div = n / 10;
+        n = div;
+    }
+    printf("Sum Of all the digit is %d", sum);
+
+    return 0;
+}
