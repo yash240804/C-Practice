077bea75f13744cc7889d0a5cdacb3735c8266cc
Yash <yashbisen24@gmail.com>
2023-12-21 21:06:55 +0530

    16th problem

---
+++ 16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+char rev(char *string)
+{
+    while ()
+    {
+        
+    }
+}
+int main()
+{
+    char s[50];
+    printf("Enter your string\n");
+    gets(s);
+    printf("Entered string is : \n");
+    puts(s);
+    rev(s);
+
+    return 0;
+}
